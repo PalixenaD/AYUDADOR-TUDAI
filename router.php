1de105fb9e5ec85660fb545be4755150a54a9757
joaquinaranzadi <aranzadijoaquin@gmail.com>
2026-05-04 16:58:55 -0300

    Actualice router

--- router.php
+++ router.php
@@ -1,7 +1,6 @@
 <?php
-require_once __DIR__ . '/app/controllers/issues.controller.php';
+require_once __DIR__ . '/app/controllers/Album.controller.php';
 
-// define la base URL del sitio
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
 /**  TABLA DE RUTEO                             
@@ -11,24 +10,25 @@ define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'
    
  **/
 
-// accion por default
 $action = 'home';
 
-// leo la accion que viene por parámetro
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
 
-// parsea la accion Ej: staff/juan --> ['staff', juan]
 $params = explode('/', $action);
 
-// rutea según la acción
 switch ($params[0]) {
     case 'home':
-        $controller = new IssuesController();
-        $controller->showAll();
+        $controller = new AlbumController();
+        $controller->home();
         break;
 
+   case 'album':
+        $id = $params[1] ?? null;
+        $controller = new AlbumController();
+        $controller->mostrarAlbum($id);
+        break;
     case 'add':
         $controller = new IssuesController();
         $controller->add();
