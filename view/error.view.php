bdaac22c964324a15a9222ddfc361a871b0b4759
PalixenaD <palixenad2@gmail.com>
2026-05-09 19:04:56 -0300

    Update error.view.php

--- view/error.view.php
+++ view/error.view.php
@@ -1,1 +1,13 @@
 
+<?php
+
+class ErrorView {
+    protected $user;
+
+    public function setUser($user) {
+        $this->user = $user;
+    }
+    public function renderError($err = null) {
+        require __DIR__ . '/templates/error.phtml';
+    }
+}
