e1a65d22374c0f0333d689ab48276a2befdf7b02
PalixenaD <palixenad2@gmail.com>
2026-05-15 16:05:48 -0300

    Update AuthController.php

--- cotroller/AuthController.php
+++ cotroller/AuthController.php
@@ -1,1 +1,42 @@
 
+<?php
+require_once __DIR__ . '/../models/users.model.php';
+require_once __DIR__ . '/../views/auth.view.php';
+require_once __DIR__ . '/../views/error.view.php';
+
+class AuthController {
+    private $view;
+    
+    public function __construct() {
+        $this->model = new UsersModel();
+        $this->view = new AuthView();
+        $this->errorView = new ErrorView();
+    }
+    
+    public function showForm($req){
+        $this->view->showForm();
+    }
+
+    public function login($req){
+        if(empty($_POST["email"]) || empty($_POST["password"]))
+            return $this->view->showForm();
+
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $user = $this->model->getByEmail($email);
+
+        if(!$user) {
+            return $this->errorView->renderError("Usuario o contraseña incorrecta");
+        }
+
+        if(!password_verify($password, $user->password)) {
+            return $this->errorView->renderError("Usuario o contraseña incorrecta");
+        }
+
+        $_SESSION["id"] = $user->id;
+        $_SESSION["email"] = $user->email;
+
+        header("Location: ". BASE_URL);
+    }
+}
