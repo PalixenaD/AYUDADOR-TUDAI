fa7ac51cc81d864effafcfc286ad9dcb96f6fb51
joaquinaranzadi <aranzadijoaquin@gmail.com>
2026-05-16 20:09:12 -0300

    Update auth.model.php

---
+++ model/auth.model.php
@@ -0,0 +1,15 @@
+<?php
+
+    class AuthModel {
+        private $db;
+
+        public function __construct() {
+        $this->db = new PDO('mysql:host=localhost;dbname=tpe_web2;charset=utf8', 'root', '');
+        }
+
+    public function getUsuario($nombre_usuario) {
+        $query = $this->db->prepare('SELECT * FROM usuario WHERE nombre_usuario = ?');
+        $query->execute([$nombre_usuario]);
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
+}
