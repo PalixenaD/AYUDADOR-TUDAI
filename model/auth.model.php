9fd6fe4c4028403265934e85e16b512b766eed39
PalixenaD <palixenad2@gmail.com>
2026-05-18 10:45:10 -0300

    Update auth.model.php

--- model/auth.model.php
+++ model/auth.model.php
@@ -10,6 +10,26 @@
     public function getUsuario($nombre_usuario) {
         $query = $this->db->prepare('SELECT * FROM usuario WHERE nombre_usuario = ?');
         $query->execute([$nombre_usuario]);
-        return $query->fetch(PDO::FETCH_OBJ);
+        $usuario = $query->fetchAll(PDO::FETCH_OBJ);
+        return $usuario;
     }
+
+
+public function get($id){
+    $query = $this->db->prepare('SELECT * FROM usuario WHERE id = ?' );
+    $query->execute([$id]);
+    return $query->fetch(PDO::FETCH_OBJ);
+
+}
+
+ public function getByEmail($email){
+        $query = $this->db->prepare('SELECT * FROM usuario WHERE email = ?')
+        $query->([$email]);
+     return $query->fetch(PDO::FETCH_OBJ);
+            
+ }
+
+
+        
+        
 }
