4f5140b719cc952756fe8d45a985c05a615a548e
PalixenaD <palixenad2@gmail.com>
2026-05-18 10:49:46 -0300

    Update session.middleware.php

--- middlewares/session.middleware.php
+++ middlewares/session.middleware.php
@@ -2,11 +2,11 @@
     class SessionMiddleware {
         public function run($req) {
             if (isset($_SESSION["id"])) {
-                $req->usuario = new StdClass();
-                $req->usuario->id = $_SESSION["id"];
-                $req->usuario->email = $_SESSION["email"];
+                $req->user = new StdClass();
+                $req->user->id = $_SESSION["id"];
+                $req->user->email = $_SESSION["email"];
             } else {
-                $req->usuario = null;
+                $req->user = null;
             }
             return $req;
         }
