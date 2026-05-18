4178673ae466940dd40b732e561d9045e78a9ef4
PalixenaD <palixenad2@gmail.com>
2026-05-18 10:56:17 -0300

    Update album.view.php

--- view/album.view.php
+++ view/album.view.php
@@ -2,7 +2,7 @@
 
 class AlbumView {
   public function renderHome($albumes) {
-    require_once __DIR__ . '/templates/JugadorList.phtml';
+    require_once __DIR__ . '/templates/Albumes.phtml';
   }
 
   public function renderAlbum($album) {
